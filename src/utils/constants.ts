a3ef02953db9577ef45ebc4d8ec32335e151ec9e
torikushiii <torikushiii@gmail.com>
2024-07-26 17:03:55 +0800

    Update Open Graph image for the website

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+import { SITE } from "../../config";
+
 export const AUTHOR = "torikushiii";
 export const THEME_COLOUR = "#dd7878";
 export const REPO_URL = "https://github.com/torikushiii/uploader";
+export const ICON = `${SITE}/favicon.ico`;
