d54995d539b09378c5b026221737ee8201652134
torikushiii <torikushiii@gmail.com>
2024-08-01 23:07:51 +0800

    linted

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
 export const ALLOWED_FILE_TYPES = [
-    'image/jpeg',
-    'image/png',
-    'image/gif',
-    'image/webp',
-    'video/mp4',
-    'video/webm',
-    'video/ogg'
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "image/webp",
+    "video/mp4",
+    "video/webm",
+    "video/ogg"
   ];
   
   export const MAX_FILE_SIZE = 100 * 1024 * 1024;
